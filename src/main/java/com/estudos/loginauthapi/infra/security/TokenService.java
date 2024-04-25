51e1f635055d1d4bd161ba8cf159ec431de06ff6
Marcos1327 <giovannygigi@gmail.com>
2024-04-24 22:23:44 -0300

    create method validateToken

--- src/main/java/com/estudos/loginauthapi/infra/security/TokenService.java
+++ src/main/java/com/estudos/loginauthapi/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.estudos.loginauthapi.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.estudos.loginauthapi.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -29,6 +30,21 @@ public class TokenService {
         }catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar token.");
         }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("login-auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException exception){
+            return null;
+        }
+
     }
 
     private Instant generateExpirationDate(){
